010cc9627ed707401a5f5ab749fd1d30f40001f7
Jimil Desai <jimildesai42@gmail.com>
2023-04-04 16:21:33 +0200

    use buckets from crds
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- backup.py
+++ backup.py
@@ -12,8 +12,9 @@ def create_cronjob(spec, body, **kwargs):
     job_name = body["metadata"]["name"]
     cron_job_name = job_name + "-cronjob"
 
+    buckets = spec["buckets"]
+    logging.info(buckets)
     # Define the CronJob object
-    # we need to pass all the rclone config and the secrets from the crd to the cronjob
     cron_job = client.V1beta1CronJob(
         api_version="batch/v1beta1",
         kind="CronJob",
@@ -38,16 +39,43 @@ def create_cronjob(spec, body, **kwargs):
                                             "memory": spec["jobResources"]["memory"],
                                         },
                                     ),
-                                    env=[client.V1EnvVar(name='RCLONE_CONFIG_MEYRIN_TYPE', value=spec['source']['remoteType']),
-                                         client.V1EnvVar(name='RCLONE_CONFIG_MEYRIN_PROVIDER', value=spec['source']['provider']),
-                                         client.V1EnvVar(name='RCLONE_CONFIG_MEYRIN_ENDPOINT', value=spec['source']['endPoint']),
-                                         client.V1EnvVar(name='RCLONE_CONFIG_S3_TYPE', value=spec['destination']['remoteType']),
-                                         client.V1EnvVar(name='RCLONE_CONFIG_S3_PROVIDER', value=spec['destination']['provider']),
-                                         client.V1EnvVar(name='RCLONE_CONFIG_S3_ENDPOINT', value=spec['destination']['endPoint']),
-                                         # we can just pass the secret name as env variable and access it from the cronjob controller script
-                                         client.V1EnvVar(name='MEYRIN_SECRET_KEY', value=spec['source']['secretName']),
-                                         client.V1EnvVar(name='PREVESSIN_SECRET_KEY', value=spec['destination']['secretName']),
-                                        ]
+                                    env=[
+                                        client.V1EnvVar(
+                                            name="RCLONE_CONFIG_MEYRIN_TYPE",
+                                            value=spec["source"]["remoteType"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="RCLONE_CONFIG_MEYRIN_PROVIDER",
+                                            value=spec["source"]["provider"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="RCLONE_CONFIG_MEYRIN_ENDPOINT",
+                                            value=spec["source"]["endPoint"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="RCLONE_CONFIG_S3_TYPE",
+                                            value=spec["destination"]["remoteType"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="RCLONE_CONFIG_S3_PROVIDER",
+                                            value=spec["destination"]["provider"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="RCLONE_CONFIG_S3_ENDPOINT",
+                                            value=spec["destination"]["endPoint"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="MEYRIN_SECRET_KEY",
+                                            value=spec["source"]["secretName"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="PREVESSIN_SECRET_KEY",
+                                            value=spec["destination"]["secretName"],
+                                        ),
+                                        client.V1EnvVar(
+                                            name="BUCKET_LIST", value=["buckets"]
+                                        ),
+                                    ],
                                 )
                             ],
                             restart_policy="Never",
