3c42a92f6ebe661ad9a078c7321b058ed3a1d15e
Jimil Desai <jimildesai42@gmail.com>
2023-04-18 16:02:55 +0200

    update image tag
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- backup.py
+++ backup.py
@@ -53,7 +53,7 @@ def create_cronjob(spec, body, **kwargs):
                             containers=[
                                 client.V1Container(
                                     name="backup",
-                                    image="inspirehep/cronjob-controller:44a01638c1b5a2bdd37706357db9f8844c1271cf",
+                                    image="inspirehep/cronjob-controller:fe27eb49e466ea1a42a88a1e4cc507b2e0edbb82",
                                     resources=client.V1ResourceRequirements(
                                         limits={
                                             "cpu": spec["jobResources"]["cpu"],
